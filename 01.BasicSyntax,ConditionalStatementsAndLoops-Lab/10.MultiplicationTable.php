809a464ef912d537b5fdbf7fd87874d130dc02a1
Kalin Motovilkov <monyllakk@abv.bg>
2019-10-29 20:42:50 +0200

    Create 10.MultiplicationTable.php

---
+++ 01.BasicSyntax,ConditionalStatementsAndLoops-Lab/10.MultiplicationTable.php
@@ -0,0 +1,10 @@
+<?php
+
+$n = intval(readline());
+$product = 0;
+for ($i = $n; $i <= $n; $i++) {
+    for ($j = 1; $j <= 10; $j++) {
+        $product = $i * $j;
+        echo "$i X $j = $product" . PHP_EOL;
+    }
+}
