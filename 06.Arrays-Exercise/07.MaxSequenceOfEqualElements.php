ea0392c0147421365ef1fd0da7c12dba109114be
Kalin Motovilkov <monyllakk@abv.bg>
2019-12-10 22:37:01 +0200

    Create 07.MaxSequenceOfEqualElements.php

---
+++ 06.Arrays-Exercise/07.MaxSequenceOfEqualElements.php
@@ -0,0 +1,30 @@
+<?php
+
+$array = array_map("intval", explode(" ", readline()));
+$maxNumbers = 1;
+$count = 1;
+$maxCount = 1;
+$pos = 0;
+
+while ($pos < count($array) - 1) {
+
+
+    if ($array[$pos] == $array[$pos + 1]) {
+        $count++;
+
+        if ($count > $maxCount) {
+            $maxCount = $count;
+            $maxNumbers = $array[$pos];
+        }
+    } else {
+        $count = 1;
+    }
+    $pos++;
+    if ($maxCount == 1) {
+        $maxNumbers = $array[0];
+
+    }
+}
+for ($i = 0; $i < $maxCount; $i++) {
+    echo "$maxNumbers ";
+}
